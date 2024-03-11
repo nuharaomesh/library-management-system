6e24fbb462f79db083177ac76ccbe6f98e45e715
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-12 01:10:41 +0530

    add user sign in form functions

--- src/main/java/lk/ijse/controller/UserSignInFormController.java
+++ src/main/java/lk/ijse/controller/UserSignInFormController.java
@@ -1,4 +1,32 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class UserSignInFormController {
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtFirstName;
+
+    @FXML
+    private TextField txtLastName;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private PasswordField txtRepeatPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnSingInOnAction(ActionEvent event) {
+
+    }
 }
