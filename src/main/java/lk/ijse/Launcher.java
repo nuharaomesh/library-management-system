0ea45e39a6ea24baa4cc00e6f9c37487b45740c8
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-11 13:21:44 +0530

    add launch class

---
+++ src/main/java/lk/ijse/Launcher.java
@@ -0,0 +1,22 @@
+package lk.ijse;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/start_form.fxml"))));
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.centerOnScreen();
+        stage.show();
+    }
+}
