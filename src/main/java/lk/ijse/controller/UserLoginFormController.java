b35f31ef5f97cfafd46307c60a3a88c11007a326
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-12 01:10:52 +0530

    add user login in form functions

--- src/main/java/lk/ijse/controller/UserLoginFormController.java
+++ src/main/java/lk/ijse/controller/UserLoginFormController.java
@@ -1,4 +1,30 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class UserLoginFormController {
+
+
+    @FXML
+    public PasswordField txtPassword;
+
+    @FXML
+    public TextField txtEmail;
+
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignInOnAction(ActionEvent event) {
+
+    }
+
+    public void btnForgetPasswordOnAction(ActionEvent actionEvent) {
+    }
 }
