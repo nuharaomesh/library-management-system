78b93e8593a021455baa25a23a597fb6c1e5b56b
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-12 11:46:22 +0530

    update user login

--- src/main/java/lk/ijse/controller/UserLoginFormController.java
+++ src/main/java/lk/ijse/controller/UserLoginFormController.java
@@ -2,26 +2,35 @@ package lk.ijse.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class UserLoginFormController {
 
-
     @FXML
     public PasswordField txtPassword;
-
     @FXML
     public TextField txtEmail;
+    @FXML
+    public AnchorPane pane;
 
 
     @FXML
-    void btnLoginOnAction(ActionEvent event) {
-
+    void btnLoginOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) this.pane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/main_form.fxml"))));
+        stage.centerOnScreen();
+        stage.show();
     }
 
     @FXML
-    void btnSignInOnAction(ActionEvent event) {
+    void btnSignInOnAction(ActionEvent event) throws IOException {
 
     }
 
