8f5f4bafa947e7ddffb5725b3add45b876d4484a
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-14 01:49:56 +0530

    add action to log in button

--- src/main/java/lk/ijse/controller/AdminLoginFormController.java
+++ src/main/java/lk/ijse/controller/AdminLoginFormController.java
@@ -3,9 +3,11 @@ package lk.ijse.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -24,8 +26,11 @@ public class AdminLoginFormController {
     }
 
     @FXML
-    void btnLoginOnAction(ActionEvent event) {
-
+    void btnLoginOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) this.pane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/admin_dashboard_form.fxml"))));
+        stage.centerOnScreen();
+        stage.show();
     }
 
     @FXML
