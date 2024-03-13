4451cefa0023400b3e0753d6fa1196614aa8367a
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-14 01:29:01 +0530

    add sign in page navigation to admin main page

--- src/main/java/lk/ijse/controller/AdminSignInFormController.java
+++ src/main/java/lk/ijse/controller/AdminSignInFormController.java
@@ -2,11 +2,19 @@ package lk.ijse.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminSignInFormController {
 
+    @FXML
+    public AnchorPane pane;
     @FXML
     private TextField txtAddress;
     @FXML
@@ -23,8 +31,11 @@ public class AdminSignInFormController {
     private TextField txtUserName;
 
     @FXML
-    void btnCreateAccount(ActionEvent event) {
-
+    void btnCreateAccount(ActionEvent event) throws IOException {
+        Stage stage = (Stage) this.pane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/user_main_form.fxml"))));
+        stage.centerOnScreen();
+        stage.show();
     }
 
     @FXML
