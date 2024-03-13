881737d8bce53982f7d0ab47353d6b17de8a5efe
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-14 00:49:27 +0530

    add admin login page function

--- src/main/java/lk/ijse/controller/AdminLoginFormController.java
+++ src/main/java/lk/ijse/controller/AdminLoginFormController.java
@@ -1,4 +1,35 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class AdminLoginFormController {
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    void btnForgetPasswordOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSignInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ckBoxShowPassword(ActionEvent event) {
+
+    }
 }
