2aba27ab71ffdef2703a5498daf1a4fcdf560716
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-13 17:10:24 +0530

    add user sign-in function to main page

--- src/main/java/lk/ijse/controller/UserSignInFormController.java
+++ src/main/java/lk/ijse/controller/UserSignInFormController.java
@@ -2,9 +2,14 @@ package lk.ijse.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class UserSignInFormController {
 
@@ -24,7 +29,10 @@ public class UserSignInFormController {
     public PasswordField txtRepeatPassword;
 
     @FXML
-    void btnSingInOnAction(ActionEvent event) {
-
+    void btnSingInOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) this.pane.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/user_main_form.fxml"))));
+        stage.centerOnScreen();
+        stage.show();
     }
 }
