627f0a113ee7641e19fe8f6dcc58136d3b80b949
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-14 01:26:16 +0530

    add sign in page functions

--- src/main/java/lk/ijse/controller/AdminSignInFormController.java
+++ src/main/java/lk/ijse/controller/AdminSignInFormController.java
@@ -1,4 +1,34 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class AdminSignInFormController {
+
+    @FXML
+    private TextField txtAddress;
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private TextField txtFirstName;
+    @FXML
+    private TextField txtLastName;
+    @FXML
+    private PasswordField txtPassword;
+    @FXML
+    private PasswordField txtRepeatPassword;
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnCreateAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ckBoxShowPassword(ActionEvent event) {
+
+    }
 }
