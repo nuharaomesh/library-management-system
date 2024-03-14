806b504f42264ebc6860576d7542f7b445d30922
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-14 20:07:22 +0530

    update book add form

--- src/main/java/lk/ijse/controller/AdminBookFormController.java
+++ src/main/java/lk/ijse/controller/AdminBookFormController.java
@@ -2,54 +2,51 @@ package lk.ijse.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
 
 public class AdminBookFormController {
 
     @FXML
+    private ComboBox<?> cmbBranch;
+    @FXML
+    private ComboBox<?> cmbCategory;
+    @FXML
     private TableColumn<?, ?> colAddress;
-
     @FXML
     private TableColumn<?, ?> colAuthor;
-
     @FXML
     private TableColumn<?, ?> colBookCount;
-
     @FXML
     private TableColumn<?, ?> colBookID;
-
     @FXML
     private TableColumn<?, ?> colBranchId;
-
     @FXML
     private TableColumn<?, ?> colBranchName;
-
     @FXML
     private TableColumn<?, ?> colCategory;
-
     @FXML
     private TableColumn<?, ?> colCount;
-
     @FXML
     private TableColumn<?, ?> colTel;
-
     @FXML
     private TableColumn<?, ?> colTitle;
-
     @FXML
     private Label lblBookTitle;
-
     @FXML
     private TableView<?> tblBookDetails;
-
     @FXML
     private TableView<?> tblBooks;
-
     @FXML
     private TextField txtBookTitleSearch;
+    @FXML
+    private Pane paneBookAdd;
+    @FXML
+    private TextField txtAuthorName;
+    @FXML
+    private TextField txtBookQty;
+    @FXML
+    private TextField txtBookTitle;
 
     @FXML
     void btnBookAddOnAction(ActionEvent event) {
@@ -68,6 +65,16 @@ public class AdminBookFormController {
 
     @FXML
     void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBookAddingOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBookAddingBackOnAction(ActionEvent event) {
 
     }
 }
