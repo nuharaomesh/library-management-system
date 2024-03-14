3bfc8db9c36ecf34779fca7127d002aae4e7b26e
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-14 13:20:44 +0530

    add func to admin book form

--- src/main/java/lk/ijse/controller/AdminBookFormController.java
+++ src/main/java/lk/ijse/controller/AdminBookFormController.java
@@ -1,4 +1,73 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class AdminBookFormController {
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colAuthor;
+
+    @FXML
+    private TableColumn<?, ?> colBookCount;
+
+    @FXML
+    private TableColumn<?, ?> colBookID;
+
+    @FXML
+    private TableColumn<?, ?> colBranchId;
+
+    @FXML
+    private TableColumn<?, ?> colBranchName;
+
+    @FXML
+    private TableColumn<?, ?> colCategory;
+
+    @FXML
+    private TableColumn<?, ?> colCount;
+
+    @FXML
+    private TableColumn<?, ?> colTel;
+
+    @FXML
+    private TableColumn<?, ?> colTitle;
+
+    @FXML
+    private Label lblBookTitle;
+
+    @FXML
+    private TableView<?> tblBookDetails;
+
+    @FXML
+    private TableView<?> tblBooks;
+
+    @FXML
+    private TextField txtBookTitleSearch;
+
+    @FXML
+    void btnBookAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBookSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
 }
