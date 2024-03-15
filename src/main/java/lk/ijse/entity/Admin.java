4171bac5e8c54952663dababe6fdc92c2230819a
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-15 10:20:49 +0530

    add admin entity

--- src/main/java/lk/ijse/entity/Admin.java
+++ src/main/java/lk/ijse/entity/Admin.java
@@ -1,4 +1,24 @@
 package lk.ijse.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Admin {
+
+    @Id
+    private String admin_id;
+    private String first_name;
+    private String last_name;
+    private String username;
+    private String email;
+    private String password;
 }
