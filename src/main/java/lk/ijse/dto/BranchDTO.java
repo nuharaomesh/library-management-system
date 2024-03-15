935643d32ebd4657e9c5dd9ecf3aa91c191309dc
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-15 18:11:21 +0530

    add BranchDTO

--- src/main/java/lk/ijse/dto/BranchDTO.java
+++ src/main/java/lk/ijse/dto/BranchDTO.java
@@ -1,4 +1,17 @@
 package lk.ijse.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class BranchDTO {
+    private String branchName;
+    private String address;
+    private int contact;
+    private boolean branchClosed;
 }
