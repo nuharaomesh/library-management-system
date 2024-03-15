793a10eabf6b81aa3fc4599cc2e5c65cf64a35c9
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-15 14:43:16 +0530

    add Transaction entity

--- src/main/java/lk/ijse/entity/Transaction.java
+++ src/main/java/lk/ijse/entity/Transaction.java
@@ -1,4 +1,29 @@
 package lk.ijse.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int transaction_id;
+    private LocalDate borrow_date;
+    private LocalDate return_date;
+    @ManyToOne
+    @JoinColumn (name = "user_id")
+    private User user;
+    @JoinColumn (name = "book_id")
+    @ManyToOne
+    private Book book;
 }
