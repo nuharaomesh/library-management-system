31886f9fc59e9e17aa179221e83f576f46c3ec8e
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-15 18:11:09 +0530

    add BookDTO

--- src/main/java/lk/ijse/dto/BookDTO.java
+++ src/main/java/lk/ijse/dto/BookDTO.java
@@ -1,4 +1,18 @@
 package lk.ijse.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class BookDTO {
+    private String title;
+    private String genre;
+    private String author;
+    private boolean returned;
+    private int count;
 }
