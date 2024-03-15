3949fd92e035b57512c559b0d91aeeda65d1dcf7
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-15 15:57:37 +0530

    init DTOs

---
+++ src/main/java/lk/ijse/dto/AdminDTO.java
@@ -0,0 +1,15 @@
+package lk.ijse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AdminDTO {
+
+
+}
