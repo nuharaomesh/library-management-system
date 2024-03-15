46962fa64163c798edecb9ff9a927e8c8f5c3075
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-15 14:42:50 +0530

    add Book entity

--- src/main/java/lk/ijse/entity/Admin.java
+++ src/main/java/lk/ijse/entity/Admin.java
@@ -1,6 +1,8 @@
 package lk.ijse.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,9 +17,11 @@ import lombok.Setter;
 public class Admin {
 
     @Id
-    private String admin_id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int admin_id;
     private String first_name;
     private String last_name;
+    private String address;
     private String username;
     private String email;
     private String password;
--- src/main/java/lk/ijse/entity/Book.java
+++ src/main/java/lk/ijse/entity/Book.java
@@ -1,4 +1,29 @@
 package lk.ijse.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int book_id;
+    private String title;
+    private String genre;
+    private String Author;
+    private boolean returned;
+    @OneToMany (mappedBy = "book")
+    private List<Transaction> transactions;
+    @ManyToMany (mappedBy = "books")
+    private List<Branch> branches;
 }
