1434de4e161cf586a34b55d9732a858471042204
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-15 18:11:30 +0530

    add TransactionDTO

--- src/main/java/lk/ijse/dto/TransactionDTO.java
+++ src/main/java/lk/ijse/dto/TransactionDTO.java
@@ -1,4 +1,19 @@
 package lk.ijse.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class TransactionDTO {
+    private LocalDate borrowDate;
+    private LocalDate returnDate;
+    private LocalDate dueDate;
+    private boolean returned;
 }
