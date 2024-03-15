80a5406f5044ada8e3d5581f39e01b464768a925
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-15 18:11:42 +0530

    add UserDTO

--- src/main/java/lk/ijse/dto/UserDTO.java
+++ src/main/java/lk/ijse/dto/UserDTO.java
@@ -1,4 +1,18 @@
 package lk.ijse.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserDTO {
+    private String first_name;
+    private String last_name;
+    private String username;
+    private String email;
+    private String password;
 }
