2713d5dd05edcdc1e2d691922626d60aa96f3e9d
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-15 14:43:06 +0530

    add Branch entity

--- src/main/java/lk/ijse/entity/Branch.java
+++ src/main/java/lk/ijse/entity/Branch.java
@@ -1,4 +1,31 @@
 package lk.ijse.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Branch {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int branch_id;
+    private String branch_name;
+    private String branch_address;
+    private String contact;
+    @ManyToMany
+    @JoinTable (
+            name = "Book_Details",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "branch_id")
+    )
+    private List<Book> books;
 }
