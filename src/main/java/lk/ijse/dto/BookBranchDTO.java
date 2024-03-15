4b8478a01b0c138da2347bb37b96646949241218
nuharaomesh <omeshnuhara4@gmail.com>
2024-03-15 18:10:58 +0530

    add BookBranchDTO

--- src/main/java/lk/ijse/dto/BookBranchDTO.java
+++ src/main/java/lk/ijse/dto/BookBranchDTO.java
@@ -1,4 +1,14 @@
 package lk.ijse.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class BookBranchDTO {
+    private int qty;
 }
